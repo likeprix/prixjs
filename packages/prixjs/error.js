f3dbaf963f0e7029e9cc18bd00219bebabd9c40f
DisamTech <disamprivate@gmail.com>
2023-03-28 16:01:16 +0200

    add: Prix error file

---
+++ packages/prixjs/error.js
@@ -0,0 +1,9 @@
+import './error.css';
+
+export default function error1() {
+    return `<div class="error-page">
+    <h1>Error</h1>
+    <p>An error has ocurred.</p>
+    <a>flying mi gente</a>
+  </div>`
+}
